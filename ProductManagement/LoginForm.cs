3f7a35a9628d5c23e577e0c8e31ca74d82dc4f93
sriharigoud087 <sriharigoud087@gmail.com>
2024-10-14 20:25:57 -0500

    first commit

---
+++ ProductManagement/LoginForm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProductManagement
+{
+    public partial class LoginForm : Form
+    {
+        private string connectionString = "Data Source=YourServer;Initial Catalog=MISDB;Integrated Security=True";
+
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "SELECT COUNT(*) FROM Users WHERE Username = @Username AND Password = @Password";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@Username", username);
+                command.Parameters.AddWithValue("@Password", password);
+
+                connection.Open();
+                int count = (int)command.ExecuteScalar();
+
+                if (count > 0)
+                {
+                    MainForm mainForm = new MainForm();
+                    mainForm.ShowDialog();
+                }
+                else
+                {
+                    MessageBox.Show("Invalid username or password");
+                }
+            }
+        }
+    }
+}
---
+++ ProductManagement/MainForm.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data;
+using System;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+
+// Program.cs
+using System;
+using System.Windows.Forms;
+namespace ProductManagement
+{
+    public partial class LoginForm : Form
+    {
+        private string connectionString = "Data Source=YourServer;Initial Catalog=ProductManagementDB;Integrated Security=True";
+
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "SELECT COUNT(*) FROM Users WHERE Username = @Username AND Password = @Password";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@Username", username);
+                command.Parameters.AddWithValue("@Password", password);
+
+                connection.Open();
+                int count = (int)command.ExecuteScalar();
+
+                if (count > 0)
+                {
+                    MainForm mainForm = new MainForm();
+                    mainForm.ShowDialog();
+                }
+                else
+                {
+                    MessageBox.Show("Invalid username or password");
+                }
+            }
+        }
+    }
+}
+
+// MainForm.cs
+
+
+namespace ProductManagement
+{
+    public partial class MainForm : Form
+    {
+        private string connectionString = "Data Source=YourServer;Initial Catalog=ProductManagementDB;Integrated Security=True";
+
+        public MainForm()
+        {
+            InitializeComponent();
+            LoadUsers();
+            LoadProducts();
+            LoadOrders();
+        }
+
+        private void LoadUsers()
+        {
+            string query = "SELECT * FROM Users";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+                dgvUsers.DataSource = table;
+            }
+        }
+
+        private void LoadProducts()
+        {
+            string query = "SELECT * FROM Products";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+                dgvProducts.DataSource = table;
+            }
+        }
+
+        private void LoadOrders()
+        {
+            string query = "SELECT * FROM Orders";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+                dgvOrders.DataSource = table;
+            }
+        }
+
+        private void btnAddProduct_Click(object sender, EventArgs e)
+        {
+            string productName = txtProductName.Text;
+            decimal price = decimal.Parse(txtPrice.Text);
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "INSERT INTO Products (ProductName, Price) VALUES (@ProductName, @Price)";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@ProductName", productName);
+                command.Parameters.AddWithValue("@Price", price);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+
+            LoadProducts();
+        }
+
+        private void btnDeleteProduct_Click(object sender, EventArgs e)
+        {
+            if (dgvProducts.SelectedRows.Count > 0)
+            {
+                int productId = Convert.ToInt32(dgvProducts.SelectedRows[0].Cells["ProductID"].Value);
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    string query = "DELETE FROM Products WHERE ProductID = @ProductID";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@ProductID", productId);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+
+                LoadProducts();
+            }
+            else
+            {
+                MessageBox.Show("Please select a product to delete.");
+            }
+        }
+    }
+}
+
+
+namespace ProductManagement
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new LoginForm());
+        }
+    }
+}
